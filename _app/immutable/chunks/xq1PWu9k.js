1f8c00ae6760deee724f0988f62ae0fcda66a06d
Andres-L1 <andreslgumuzio@gmail.com>
2025-12-25 15:26:41 +0100

    Updates

--- _app/immutable/chunks/xq1PWu9k.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1v76p16)==null?void 0:s.base)??"/Grandmaster-Fantasy";var a;const e=((a=globalThis.__sveltekit_1v76p16)==null?void 0:a.assets)??t??"";export{e as a,t as b};
--- _app/immutable/entry/start.VWjVqSZV.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DhR0a2MA.js";export{o as load_css,r as start};
