a56ff9207fad8146a65c5dc19fe28749a7c36d5d
Andres-L1 <andreslgumuzio@gmail.com>
2025-12-25 14:17:14 +0100

    Updates

--- _app/immutable/chunks/YfyHElg8.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1v3y6gu)==null?void 0:s.base)??"/Grandmaster-Fantasy";var a;const e=((a=globalThis.__sveltekit_1v3y6gu)==null?void 0:a.assets)??t??"";export{e as a,t as b};
