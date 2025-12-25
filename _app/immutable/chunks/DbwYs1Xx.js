227bf0b761c1d95e8fe9e05203a975fd4afe21a5
Andres-L1 <andreslgumuzio@gmail.com>
2025-12-25 18:00:56 +0100

    Updates

---
+++ _app/immutable/chunks/DbwYs1Xx.js
@@ -0,0 +1,1 @@
+function s(c){return--c*c*((1.70158+1)*c+1.70158)+1}function t(c){const n=c-1;return n*n*n+1}export{s as b,t as c};
--- _app/immutable/entry/start.CmX8XcG3.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BrUyRPvE.js";export{o as load_css,r as start};
